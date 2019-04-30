1169456304619e890be9c131a70672806edce1d1
canmanietp <c.t.ponnambalam@tudelft.nl>
2019-04-30 12:03:26 +0200

    init

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-import Simple_Q
+import SARSA
 
 def moving_average(a, n=3):
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
-q_rewards = Simple_Q.run(50000)
-q_plot = moving_average(q_rewards, 5000) # smooth plot with a moving average
-plt.plot(q_plot)
+S_rewards = SARSA.run(50000)
+S_plot = moving_average(S_rewards, 5000) # smooth plot with a moving average
+plt.plot(S_plot)
 plt.show()
